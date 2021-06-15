d3de9f02eb99856d36ea876d59e21a63cbe95503
boyplus <ict224bj@gmail.com>
2021-06-15 12:12:19 +0700

    init: annotations

---
+++ features/annotations/variables.js
@@ -0,0 +1,26 @@
+var apples = 5;
+var speed = "fast";
+var nothingMuch = null;
+var nothing = undefined;
+// built in objects
+var now = new Date();
+// Array
+var colors = ['red', 'green', 'blue'];
+var myNumbers = [1, 2, 3];
+var truths = [true, false, true];
+// Classes
+var Car = /** @class */ (function () {
+    function Car() {
+    }
+    return Car;
+}());
+var car = new Car();
+// Object literal
+var point = {
+    x: 10,
+    y: 20
+};
+// Function
+var logNumber = function (i) {
+    console.log(i);
+};
