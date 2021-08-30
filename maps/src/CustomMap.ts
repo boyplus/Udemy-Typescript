67ce6bcd310ae1c613566e3641e3f3eee218924e
boyplus <ict224bj@gmail.com>
2021-08-30 23:58:25 +0700

    complete: map project

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -1,21 +1,22 @@
 //instrcutions to every other classes
 //on how they can be anargument to 'addMarker'
-interface Mappable{
+export interface Mappable{
   location:{
     lat: number;
     lng: number;
   };
   markerContent(): string;
+  color: string;
 }
 
 //we can also use class as a type like interface 
-export class CustomMap{
+export class CustomMap {
   private googleMap: google.maps.Map;
 
-  constructor(divId: string){
+  constructor(divId: string) {
     this.googleMap = new google.maps.Map(
       document.getElementById(divId),
-      { 
+      {
         zoom:1,
         center: {
           lat: 0,
@@ -25,7 +26,7 @@ export class CustomMap{
     );
   }
 
-  addMarker(mappable: Mappable): void{
+  addMarker(mappable: Mappable): void {
     const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
@@ -34,9 +35,7 @@ export class CustomMap{
       }
     })
 
-
-
-    marker.addListener('click',()=>{
+    marker.addListener('click',() => {
       const infoWindow  = new google.maps.InfoWindow({
         content: mappable.markerContent()
       })
