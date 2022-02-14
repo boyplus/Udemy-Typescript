c6e1eef5139ab89fa8c7b929ea1e3298fd8e6115
Thanaphon Sombunkaeo <ict224bj@gmail.com>
2022-02-14 07:32:31 +0200

    update: view model

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,5 +1,12 @@
 import { UserForm } from './views/UserForm';
+import { User } from './models/User';
 
-const userForm = new UserForm(document.getElementById('root'));
+const user = User.buildUser({ name: 'NAME', age: 20 });
+const root = document.getElementById('root');
 
-userForm.render();
+if (root) {
+  const userForm = new UserForm(root, user);
+  userForm.render();
+} else {
+  throw new Error('Root element not found');
+}
