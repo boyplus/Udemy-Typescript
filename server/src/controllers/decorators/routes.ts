3dae99d100d7a435a8606e1acf5625ccd923aafc
Thanaphon Sombunkaeo <ict224bj@gmail.com>
2022-02-20 03:49:23 +0200

    complete: server

--- server/src/controllers/decorators/routes.ts
+++ server/src/controllers/decorators/routes.ts
@@ -2,10 +2,15 @@ import 'reflect-metadata';
 
 import { Methods } from './Methods';
 import { MetaDataKeys } from './MetaDetaKeys';
+import { RequestHandler } from 'express';
+
+interface RouteHandlerDescriptor extends PropertyDescriptor {
+  value?: RequestHandler;
+}
 
 function routeBinder(method: string) {
   return function (path: string) {
-    return function (target: any, key: string, desc: PropertyDescriptor) {
+    return function (target: any, key: string, desc: RouteHandlerDescriptor) {
       Reflect.defineMetadata(MetaDataKeys.path, path, target, key);
       Reflect.defineMetadata(MetaDataKeys.method, method, target, key);
     };
